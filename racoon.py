1b0e25f91a80508e3c78fac27247c1ad403ea4f4
Modar Moalla <64635289+MoModar@users.noreply.github.com>
2021-08-08 17:32:26 +0200

    Add files via upload

---
+++ racoon.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Racoon:
+    def __init__(self, screen):
+        """Initialize the racoon and its starting position."""
+        self.screen = screen
+        # Load the racoon and get its rect.
+        self.image = pygame.image.load('images/racoon.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = self.screen.get_rect()
+        # Start position is the center of the left edge.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.midleft = self.screen_rect.midleft
+
+    def blitme(self):
+        """Draw the racoon at its current position"""
+        self.screen.blit(self.image, self.rect)
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for The Racoon Fighter."""
+    def __init__(self):
+        """Initialize the game settings"""
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (253, 252, 250, 255)
