f03b791069132c408bcb30ff00a92b988f85ee6e
Modar Moalla <64635289+MoModar@users.noreply.github.com>
2021-09-27 10:24:40 +0200

    Rename magic_number.py to Chapter 5/magic_number.py

---
+++ Chapter 5/magic_number.py
@@ -0,0 +1,3 @@
+answer=17
+if answer != 42:
+    print("The answer is not correct. Please try again")
--- magic_number.py
+++
@@ -1,3 +0,0 @@
-answer=17
-if answer != 42:
-    print("The answer is not correct. Please try again")
